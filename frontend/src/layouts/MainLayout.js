f197ec2c5b6fe8fdbd6f62b8878d7546f2aeb525
theethawat <5910110150@psu.ac.th>
2022-03-04 09:53:31 +0700

    Initial Commit

---
+++ frontend/src/layouts/MainLayout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const MainLayout = ({ children }) => {
+  return (
+    <div>
+      MainLayout
+      {children}
+    </div>
+  )
+}
+
+MainLayout.propTypes = {}
+
+export default MainLayout
